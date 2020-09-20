af8f094e0b4f2b4a07e16abeb74a351ff9fae22e
Rreboot8x5a <44650226+Rreboot8x5a@users.noreply.github.com>
2020-09-20 13:06:00 +0200

    Final push of this script

--- personal_code/Generate_and_visualise.py
+++ personal_code/Generate_and_visualise.py
@@ -1,13 +1,13 @@
-# #<---Click this triangle to load python libraries/ download CPlantBox from GitHub/ Make a plant and visualize it
-#############loading other python library or packages. Only need to run once at the start.##########
+"""
+This script is the one using for testing the code. The only one useful to run (as the other ones are just function script)
+It uses the basic function of this work to plot several results.
+If you do not want your computer to die because of too much plot windows opened, please use the debug mode to stop the code at certain points
+"""
 import os
 import sys
 import vtk
 sys.path.append("..")
 import plantbox as pb  # CPlantBox Python Binding
-# from vtk.util import numpy_support as VN
-# import tutorial.examples.python.vtk_plot as vp
-# from tutorial.examples.python.vtk_tools import *
 
 sys.path.append("../tutorial")
 from jupyter.CPlantBox_PiafMunch import *
@@ -17,187 +17,166 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from Get_real_values import *
+from TimeEvolution import *
 # plotly.__version__
 
 
 time = 50  # how many days the plant need to grow, make it smaller, for example 15 to see if the plant becomes smaller
 
 
-# plant = create_basil()
-# plant.initialize(True)
-# listPlant = list()
-# plantAllTime = {}
-# for i in range(1, time):
-#     plant.simulate(1)
-#     listPlant.append(plant.copy())
-#     plantDict = get_real_parameters(plant)
-#     plantAllTime[i] = plantDict
-# fileName = 'Real_parameters_of_plant.txt'
-# write_real_parameters_file(fileName, plantAllTime)
-#
-# success = read_real_parameters_file('Real_parameters_of_plant.txt')
-# isASuccess = (success == plantAllTime)
-# print('ppp')
-# # Rename file that ara wrongly named
-# # i = 47
-# # while i >= 0:
-# #     old = "/home/roberto/Téléchargements/CPlantBox-vierge/ArabidopsisData/arabidopsis_coordinates_t" + str(i) + ".txt"
-# #     new = "/home/roberto/Téléchargements/CPlantBox-vierge/ArabidopsisData/arabidopsis_coordinates_t" + str(i+1) + ".txt"
-# #     os.rename(old, new)
-# #     i -= 1
-# # print('yeah')
-#
-#
-# # for dt in range(1, int(time)):
-# #     plant1 = CPlantBox(name, dt, name) # make a plant object in python
-# #     # Visualization
-# #     fig = visual_plant(plant1)
-# #     fig.show()
-#
-# # plant1 = CPlantBox(name, 75, name)  # make a plant object in python
-# # # argh = CPlantBox_PiafMunch(name, 50, name)
-# # # Visualization
-# # fig = visual_plant(plant1)
-# # fig.show()
-# # # # Congrats! Now you are ready to use the CPlantBox
-#
-# # for dt in range(1, int(time)):
-# #     plant1 = CPlantBox(name, dt*5, name)  # make a plant object in python
-# #     fig = visual_plant(plant1)
-# #     fig.show()
-#
-# # plant = create_basil()
-# # plant = create_arabidopsis()
-# plant = creacrea()
-# plant.initialize(True)
-# plant.simulate(time)
-# fig = visual_plant(plant)
-# fig.show()
-# # test = plant.getOrganRandomParameter(3)
-# # test[0].lmax = 50
-# # test2 = plant.getOrganRandomParameter(3)
-# plant.simulate(time-5)
-# fig = visual_plant(plant)
-# fig.show()
-# for i in range(5):
-#     plant.simulate(1)
-#     fig = visual_plant(plant)
-#     fig.show()
-# test = plant.getOrganRandomParameter(3)
-# test[0].lmax = 50
-# test2 = plant.getOrganRandomParameter(3)
-# plant.simulate(30)
-# fig = visual_plant(plant)
-# fig.show()
-# for i in range(5):
-#     # plant.simulate((i+1)/2)
-#     plant.simulate(i+1)
-#     fig = visual_plant(plant)
-#     fig.show()
-# print('Finito')
-#
-# # # If you want to plot using vtk then simulate the camera recording
-# # plot_plant(plant, "creationTime", plantType="basil")
-# # coords = camera_view(plant, 50, [1280, 720], 50, False)
-# # print('debug stop')
-# # file = open("basil_coordinates.txt", "w")
-# # mot = []
-# # for coord in coords:
-# #     file.write(str(coord[0]) + ',' + str(coord[1]) + ',' + str(coord[2]) + '\n')
-# # file.close()
-#
-# # # If you want to simulate the camera view on each dt step and then write the result in a txt file
-# # for dt in range(1, time):
-# #     plant1 = create_basil()
-# #     plant1.initialize(True)
-# #     plant1.simulate(dt)
-# #     coords = vp.camera_view(plant1, 50, [1280, 720], 50, False)
-# #     # file = open("BasilData/basil_coordinates_t{}.txt".format(dt), "w")
-# #     file = open("Arabidopsis/basil_coordinates_t{}.txt".format(dt), "w")
-# #     mot = []
-# #     for coord in coords:
-# #         file.write(str(coord[0]) + ',' + str(coord[1]) + ',' + str(coord[2]) + '\n')
-# #     file.close()
-#
-# # plant1 = creacrea()
-# plant1 = create_basil()
-# plant1.initialize(True)
-# listPlant = []
-# for dt in range(1, time):
-#     plant1.simulate(1)  # Simulate one time step further than before
-#     listPlant.append(plant1.copy())
-#
-#
-# fig = visual_plant(listPlant[-1])
-# fig.show()
-#
-# print('mate kudasai')
-# plot_plant(listPlant[-1], "plant", plantType="basil", stamp=1)
-#
-# temp_coordinates = python_nodes(listPlant[-1])
-# y_min = np.amin(temp_coordinates[:, 4])
-# y_max = np.amax(temp_coordinates[:, 4])
-# z_min = np.amin(temp_coordinates[:, 5])
-# z_max = np.amax(temp_coordinates[:, 5])
-#
-# for j in range(len(listPlant)):
-#     plant = listPlant[j]
-#     coordinates = python_nodes(plant)
-#     labels = list()
-#     for i in range(len(coordinates[:, 1])):
-#         if coordinates[i, 1] == 2.0:
-#             labels.append("wheat")
-#         elif coordinates[i, 1] == 3.0:
-#             labels.append("darkgreen")
-#         elif coordinates[i, 1] == 4.0:
-#             labels.append("lightgreen")
-#         else:
-#             labels.append("black")
-#     plt.scatter(x=coordinates[:, 4], y=coordinates[:, 5], c=labels)
-#     plt.ylabel('z axis')
-#     plt.xlabel('y axis')
-#     plt.title('Growth of basil plant')
-#     plt.xlim(y_min, y_max)
-#     plt.ylim(z_min, z_max)
-#     plt.savefig("/home/roberto/Téléchargements/CPlantBox-vierge/Plot/basil_growth_{}".format(j+1))
-#     # fig = visual_plant(plant)
-#     # fig.show()
-#
-# for i in range(len(listPlant)):
-#     plot_plant(listPlant[i], "plant", plantType="basil", stamp=i+1)
-# print('tchioto')
+def for_the_presentation():
+    t = 50
+    height = 75
+    trueLength = height * math.tan(37.5 * math.pi / 180) * 2
+
+    # Plot the nodes of the basil plant object
+    basil = create_basil()
+    basil.initialize(True)
+    basil.simulate(t)
+    fig = visual_plant(basil)
+    fig.show()
+
+    # Read the txt file with the ray tracing data of the basil plant after 49 days of simulation
+    coord_basil = read_data(49, 'basil2')
+    plot_data(coord_basil[:-2], coord_basil[:-2, 2])  # The last point is 0,0,0 and the previous one is the camera position
+    basil_leaves, nb_classes, pred = get_leaves(coord_basil)
+    coord_basil = get_numpy_array(basil_leaves)
+    plot_data(coord_basil, coord_basil[:, 3])
+
+    # Plot the nodes of the arabidopsis plant object
+    arab = create_arabidopsis()
+    arab.initialize(True)
+    arab.simulate(t)
+    fig = visual_plant(arab)
+    fig.show()  # Need to be done several times because of the randomization
+
+    # Read the txt file with the ray tracing data of the arabidopsis plant after 49 days of simulation
+    coord_arab = read_data(626, 'arabidopsis')  # The 626 is there only because the txt 626 file is an experimental one
+    plot_data(coord_arab[:-2], coord_arab[:-2, 2]) # The last point is 0,0,0 and the previous one is the camera position
+    arab_leaves, nb_classes, pred = get_leaves(coord_arab, trueLength/640)
+    coord_arab = get_numpy_array(arab_leaves)
+    plot_data(coord_arab, coord_arab[:, 3])
+
+    # This function is a bit buggy with the debugging mode of pycharm, so it is put at the end of the function
+    # to avoid annoying problem
+    plot_plant(basil, "Basil plot", plantType="basil")  # Plot in 3D using VTK
+    plot_plant(arab, "Arabidopsis plot", plantType="arabidopsis")
 
 
-plant1 = create_basil()
-plant1.initialize(True)
-listPlant = []
-plantAllTime = {}
-for dt in range(1, time):
-    plant1.simulate(1)  # Simulate one time step further than before
-    listPlant.append(plant1.copy())
-    plantDict = get_real_parameters(plant1)
-    plantAllTime[dt] = plantDict
+def multiple_height_test():
+    # This function was made to plot the results of clustering with different height in the camera position
+    resolution = [640, 576]
+    height = [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125]
+    trueLength = []
+    nb_points = []
 
-plot_plant(listPlant[-1], "plant", plantType="basil")
+    basilList = []
+    basil = create_basil()
+    basil.initialize(True)
+    for j in range(10):
+        basil.simulate(5)
+        basilList.append(basil.copy())
 
-fileName = 'Real_parameters_of_basil2.txt'
-write_real_parameters_file(fileName, plantAllTime)
+    for i in range(len(height)):
+        trueLength.append(height[i] * math.tan(37.5 * math.pi / 180) * 2)
+    for i in range(len(height)):
+        for j in range(len(basilList)):
+            coords = camera_view(basilList[j], height=height[i], resolution=resolution, trueLength=trueLength[i],
+                                 isPlotted=False, plantType="basil")
+            nb_points.append(len(coords))
+            file = open("/home/roberto/Téléchargements/CPlantBox-vierge/Height/basil_coordinates_height{}_t{}.txt".format(height[i], (j + 1) * 5), "w")
+            for coord in coords:
+                file.write(str(coord[0]) + ',' + str(coord[1]) + ',' + str(coord[2]) + '\n')
+            file.close()
 
-success = read_real_parameters_file(fileName)
-isASuccess = (success == plantAllTime)
 
-# resolution = [640, 576]
-resolution = [1280, 720]
-height = 50
-# trueLength = height * math.tan(37.5 * math.pi / 180) * 2  # Multiplied by 2 because we divided the isosceles triangle into 2 rectangle triangle
-trueLength = 50
+def plot_clustering():
+    # This function was made to plot the results of clustering with different height in the camera position
+    # height = [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+    # height = [105, 110, 115, 120, 125]
+    height = [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125]
+    nb_point = []
+    trueLength = []
+    dpList = []
+    for i in range(len(height)):
+        trueLength.append(height[i] * math.tan(37.5 * math.pi / 180) * 2)
+    for i in range(len(height)):
+        nb_point_per_height = []
+        dp = trueLength[i] / 640
+        dpList.append(dp)
+        for j in range(10):
+            ncoord = read_data([height[i], (j+1)*5], 'height')
+            nb_point_per_height.append(len(ncoord))
+            leaves, nb_classes, pred = get_leaves(ncoord, dp)
+            ncoord2 = get_numpy_array(leaves)
+            # if (height[i] == 50 or height[i] == 125) and j == 9:
+            if j == 9 and height[i] % 25 == 0:
+                plot_data(ncoord[:-2], ncoord[:-2, 2])
+                plot_data(ncoord2, ncoord2[:, 3])
+            # if j == 2 or j == 4 or j == 7 or j == 9:
+            if j == 4 or j == 9:
+                print('debug stop')
+            if i % 5 == 0 and j == 9:
+                print('pause')
+        nb_point.append(nb_point_per_height)
 
-for i in range(len(listPlant)):
-    coords = camera_view(listPlant[i], height=height, resolution=resolution, trueLength=trueLength, isPlotted=False, plantType="basil")
-    # file = open("/home/roberto/Téléchargements/CPlantBox-vierge/basiltest3/basil_coordinates_t{}.txt".format(i+1), "w")
-    # file = open("/home/roberto/Téléchargements/CPlantBox-vierge/ArabidopsisData/arabidopsis_coordinates_t{}.txt".format(i+1), "w")
-    file = open("/home/roberto/Téléchargements/CPlantBox-vierge/BasilData2/Basil2_coordinates_t{}.txt".format(i + 1), "w")
-    mot = []
-    for coord in coords:
-        file.write(str(coord[0]) + ',' + str(coord[1]) + ',' + str(coord[2]) + '\n')
-    file.close()
+
+def get_dp_list():
+    height = (np.arange(40) + 1) * 5 + 45
+    trueLength = []
+    dpList = []
+    for i in range(len(height)):
+        trueLength.append(height[i] * math.tan(37.5 * math.pi / 180) * 2)
+    for i in range(len(height)):
+        dp = trueLength[i] / 640
+        dpList.append(dp)
+    temp = np.append(height[np.newaxis].T, np.array(dpList)[np.newaxis].T, axis=1)
+    return np.append(temp, np.array(dpList)[np.newaxis].T * 4, axis=1)
+
+
+def make_a_full_simulation():
+    resolution = [1280, 720]
+    height = 50
+    trueLength = 50
+
+    # Generate the plant
+    plant = create_basil()
+    plant.initialize(True)
+    listPlant = list()
+    plantAllTime = {}
+    for i in range(1, time):
+        plant.simulate(1)
+        listPlant.append(plant.copy())
+        plantDict = get_real_parameters(plant)
+        plantAllTime[i] = plantDict
+
+    # Retrieve real values of phenotype
+    fileName = 'path/name_wanted.txt'
+    write_real_parameters_file(fileName, plantAllTime)
+
+    success = read_real_parameters_file('Real_parameters_of_plant.txt')
+    isASuccess = (success == plantAllTime)  # Just to check on debug mod if there was no error with the file writing
+
+    # Making the ray tracing
+    for i in range(len(plantDict)):
+        coords = camera_view(plantDict[i], height=height, resolution=resolution, trueLength=trueLength,
+                             isPlotted=False, plantType="basil")
+        file = open("path/name_wanted_t{}.txt".format(i+1), "w")
+        for coord in coords:
+            file.write(str(coord[0]) + ',' + str(coord[1]) + ',' + str(coord[2]) + '\n')
+        file.close()
+
+    # Retrieve the phenotype by the camera view
+    simulation(time)  # Need to change lines 75 and 205 of TimeEvolution to have the correct path of file
+
+
+def main():
+    for_the_presentation()
+    make_a_full_simulation()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
